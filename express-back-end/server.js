a339600e07f5e779eb293b5c61d844731e37b42b
Shivani Sharma <shivani1406sharma@gmail.com>
2022-02-14 17:53:11 +0000

    added routes to express-back-end

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -6,6 +6,7 @@ const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
 const morgan = require("morgan");
+const cookieSession = require('cookie-session');
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
@@ -18,10 +19,27 @@ const db = new Pool(dbParams);
 // console.log(db);
 db.connect();
 
+App.use(cookieSession({
+  name: 'session',
+  keys: ["Secret, Secret, so many secrets", "key"],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 // Sample GET route
 App.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
 }));
+
+// Separated Routes for each Resource
+const indexRoutes = require("./routes/index");
+const propertiesRoutes = require("./routes/properties");
+const imagesRoutes = require("./routes/images");
+const reservationsRoutes = require("./routes/reservations");
+
+indexRoutes(db,App);
+propertiesRoutes(db,App);
+imagesRoutes(db, App);
+reservationsRoutes(db,App);
+
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
