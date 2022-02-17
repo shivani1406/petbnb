826e66b8185bb97cfcb75d9a5a28fd88cdffcff6
Shivani Sharma <shivani1406sharma@gmail.com>
2022-02-17 12:34:28 -0500

    updated app.js

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import './App.css';
 import HomePage from './HomePage';
 import Navigation from './NavBar/NavBar';
@@ -8,7 +8,8 @@ import admin from './Admin/admin';
 import createProperty from './create_property';
 import Footer from './Footer';
 import history from './history';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from "./components/navbar.component";
 import Login from './components/login.component';
 import SignUp from './components/signup.component';
@@ -20,7 +21,7 @@ class App extends Component {
       
       <div className="App">
         
-        <Router history={history}>
+        <BrowserRouter history={history}>
         <Nav/>
 
         <Switch>
@@ -31,7 +32,7 @@ class App extends Component {
            <Route path="/admin" component={admin} /> 
            <Route path="/createProperty" component={createProperty} /> 
         </Switch>
-        </Router>
+        </BrowserRouter>
         <Footer />
       </div>
     );
