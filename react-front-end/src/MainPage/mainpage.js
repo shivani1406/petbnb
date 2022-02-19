e78f1626d34abfa67c74b23113292d111c0a9683
Shivani Sharma <shivani1406sharma@gmail.com>
2022-02-19 01:02:11 -0500

    added link to create property

--- react-front-end/src/MainPage/mainpage.js
+++ react-front-end/src/MainPage/mainpage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'; //optional
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from 'axios';
 import PropertyTile from '../components/property-tile';
 
 export default function mainpage(){
 
   
-    const [query, setquery] = useState("");
+    const [searchquery, setSearch] = useState("");
     const [properties, setproperties] = useState([]);
     const baseUrl = 'http://localhost:8080';
     // const getPropertyInfo = async () => {
@@ -25,10 +25,29 @@ export default function mainpage(){
         );
       }) 
     }
+    const getSearchInfo = () => {
+      let items = {searchquery};
+      console.log(searchquery);
+      console.warn("item",items);
+      axios.get(`${baseUrl}/api/search`,items)
+       // You can simply make your requests to "/api/whatever you want"
+      .then((response) => {
+        // handle success
+        
+        console.log(response.data) // The entire response from the Rails API
+
+        setproperties(
+        response.data
+        );
+      }) 
+    }
     const onSubmit = (e) => {
       e.preventDefault();
-      getPropertyInfo();
+      getSearchInfo();
     };
+    useEffect(() => {
+      getPropertyInfo();
+    }, []);
 
     const proper = properties.map((property) => {
       return (
@@ -49,8 +68,8 @@ export default function mainpage(){
           type="text"
           placeholder="enter location"
           autoComplete="Off"
-          value={query}
-          onChange={(e) => setquery(e.target.value)}
+          value={searchquery}
+          onChange={(e) => setSearch(e.target.value)}
         />
         <input className="app__submit" type="submit" value="Search" />
       </form>
