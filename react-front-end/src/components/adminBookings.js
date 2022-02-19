ec3094fcecbc403eca875764f92aed4e9df55b26
Shivani Sharma <shivani1406sharma@gmail.com>
2022-02-19 06:09:24 -0500

    added details code for adminBooking

--- react-front-end/src/components/adminBookings.js
+++ react-front-end/src/components/adminBookings.js
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { Component } from 'react'; //optional
+import { useState , useEffect} from "react";
+import axios from 'axios';
+import PropertyList from '../components/property-list';
+import createProperty from '../create_property';
+import history from '../history';
+import { Link } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 export default function adminBookings(){
+    let {id} = useParams();
+    const [query, setquery] = useState("");
+      const [properties, setproperties] = useState([]);
+      const baseUrl = 'http://localhost:8080';
+      const getPropertyInfo = () => {
+        axios.get(`${baseUrl}/api/reservations`) // You can simply make your requests to "/api/whatever you want"
+        .then((response) => {
+          // handle success
+          console.log(response.data) // The entire response from the Rails API
+  
+          setproperties(
+          response.data
+          );
+        }) 
+      }
+      
+      useEffect(() => {
+        getPropertyInfo();
+      }, []);
+
+      const proper = properties.map((property) => {
+        return (
+          
+          <tr key={property.property_id}>
+         <td> <img className="property__img " src={property.image} /></td>
+           <td>{property.property_name}
+           <p>{property.description}</p></td>
+          
+           <td>
+             {property.user_name}
+           </td>
+         </tr>
+          
+        );
+      });
   return(
     <div className="app__createproperty">
-<h2>in admin</h2>
+<table className="table" >
+      <thead>
+        <tr>
+        <th>Image</th>
+        <th>Property Name/ Details</th>
+        <th>Guest Name</th>
+        </tr>
+        </thead>
+        <tbody>
+        {proper}
+        </tbody>
+      </table>
+      
     </div>
   );
 }
