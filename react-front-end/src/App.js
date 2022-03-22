910f727d791421c2f788eef87027a01a0183c55e
Shivani Sharma <shivani1406sharma@gmail.com>
2022-03-22 02:03:35 -0400

    added to app.js

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -22,6 +22,7 @@ import Login from './components/login.component';
 import SignUp from './components/signup.component';
 import PropertyPage from './components/propertyPage';
 import AdminBookings from './components/adminBookings';
+import userProfile from './components/userProfile';
 class App extends Component {
 
   render() {
@@ -42,6 +43,7 @@ class App extends Component {
             <Route path="/adminBookings/*" element={<AdminBookings />} />
             <Route path="/propertyDetails/:id" element={<PropertyDetails />} />
             <Route path="/myBookings/" element={<MyBookings />} />
+            <Route path="/userProfile" element={<userProfile/>} />
           </Routes>
         </BrowserRouter>
         <Footer />
