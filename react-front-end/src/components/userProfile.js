327137dd0cd2dce47155513b9453cb1fc19666a1
Shivani Sharma <shivani1406sharma@gmail.com>
2022-03-22 02:00:48 -0400

    updated profile view

--- react-front-end/src/components/userProfile.js
+++ react-front-end/src/components/userProfile.js
@@ -3,6 +3,16 @@ import React, { useState} from "react";
 import { useNavigate} from "react-router-dom";
 
 export default function userProfile() {
+  const navigate = useNavigate();
+  let owner = false;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  return(
+
+ 
   <div>
 <ul class="nav nav-tabs">
   <li class="nav-item">
@@ -27,7 +37,38 @@ export default function userProfile() {
 </ul>
 <div id="myTabContent" class="tab-content">
   <div class="tab-pane fade active show" id="home">
-    <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+    <p>  <form >
+      <img src={avatar} className="propertyTile__img" alt=""/>
+                        <div className="form-group">
+                            <label>Name</label>
+                            <input type="text" className="form-control" placeholder="Enter Name"
+                                 value={name}
+                                 onChange={(event) => {
+                                   setName(event.target.value);
+                                 }} />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input type="email" className="form-control" placeholder="Enter email"
+                                 value={email}
+                                 onChange={(event) => {
+                                   setEmail(event.target.value);
+                                 }}/>
+                        </div>
+
+                        <div className="form-group">
+                            <label>Add Avatar</label>
+                            <input type="text" className="form-control" placeholder="Enter url"
+                                 value={avatar}
+                                 onChange={(event) => {
+                                   setAvatar(event.target.value);
+                                 }} />
+                        </div>
+                        <button type="submit" className="btn btn-dark btn-lg btn-block"  onClick={registerform}>Update</button>
+
+</form>
+                        </p>
   </div>
   <div class="tab-pane fade" id="profile">
     <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit.</p>
@@ -40,5 +81,5 @@ export default function userProfile() {
   </div>
 </div>
   </div>
-  
+   )
 }
