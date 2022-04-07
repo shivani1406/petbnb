8a5a5f522fd0252c0aea9d0ab9ba91993c60ddf4
Shivani Sharma <shivani1406sharma@gmail.com>
2022-04-07 06:43:09 -0400

    added message page initial commit

---
+++ react-front-end/src/components/messages.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'; 
+const Messages = () => {
+  return(
+    <div className="app__searchForm">
+      My Message
+    </div>
+  )
+}
+
+export default Messages;
