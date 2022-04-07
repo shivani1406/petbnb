6c218006109529ebb020b8be0cb40556d02961c1
Shivani Sharma <shivani1406sharma@gmail.com>
2022-04-07 14:13:51 -0400

    added input bar

--- react-front-end/src/components/messages.js
+++ react-front-end/src/components/messages.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react'; 
+
+import Input from './input';
 import './messages.css';
 const Messages = () => {
+
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+      // socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
   return(
     <div className="app__searchForm">
       <div className="outerContainer">
       <div className="container">
           {/* <InfoBar room={room} />
-          <Messages messages={messages} name={name} />
-          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} /> */}
+          <Messages messages={messages} name={name} />*/}
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} /> 
       </div>
       {/* <TextContainer users={users}/> */}
     </div>
