62b0db46ca422eef94949393cc789008a72b2b8e
Shivani Sharma <shivani1406sharma@gmail.com>
2022-04-14 04:28:04 -0400

    added display for datepicker

--- react-front-end/src/MainPage/mainpage.js
+++ react-front-end/src/MainPage/mainpage.js
@@ -1,5 +1,7 @@
 import React from 'react'; //optional
 import { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import axios from 'axios';
 import PropertyTile from '../components/property/property-tile';
 import SimpleMap from '../simpleMaps';
@@ -7,7 +9,8 @@ import SimpleMap from '../simpleMaps';
 
 export default function mainpage(){
 
-  
+  const [startDate, setStartDate] = useState(new Date());
+  const [EndDate, setEndDate] = useState(new Date());
     const [searchquery, setSearch] = useState("");
     const [properties, setproperties] = useState([]);
     const baseUrl = 'http://localhost:8080';
@@ -64,15 +67,33 @@ export default function mainpage(){
     <div className="search_page">
       
       <form className="app__searchForm" onSubmit={onSubmit}>
+      <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
         <input
-          className="app__input"
+          className="form-control search-slt"
           type="text"
           placeholder="enter location"
           autoComplete="Off"
           value={searchquery}
           onChange={(e) => setSearch(e.target.value)}
         />
+         </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+         <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} 
+         />
+         </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                        <DatePicker selected={EndDate} onChange={(date) => setEndDate(date)} 
+         />
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
         <input className="app__submit" type="submit" value="Search" />
+        </div>
+                    </div>
+                </div>
+            </div>
       </form>
       
 <div className="app_saperator">
